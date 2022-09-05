1a6da57b985581cf0070b46f6d9d62c8244fdabf
farhodjon <farhodjon23@outlook.com>
2022-09-05 07:19:44 +0300

    Boshlandi

---
+++ shopping/src/data.js
@@ -0,0 +1,43 @@
+export const sliderItems=[
+
+{
+    id:1,
+    img:"https://picsum.photos/150/200",
+    title:'SUMMER SALE',
+    desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg:'f5fafd'
+},
+{
+    id:2,
+    img:"https://i.ibb.co/DG69bQ4/2.png",
+    title:'AUTUMN COLLECTION',
+    desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg:'fcf1ed'
+},
+{
+    id:3,
+    img:"https://picsum.photos/150/201",
+    title:'LOUNGEWEAR LOVE',
+    desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg:'fbf0f4'
+}];
+
+export const categories=[
+
+{
+    id:1,
+    img:'https://picsum.photos/150/202"',
+    title:"SHIRT STYLE!"
+},
+{
+    id:2,
+    img:'https://picsum.photos/150/203"',
+    title:"SHIRT STYLE!"
+},
+{
+    id:3,
+    img:'https://picsum.photos/150/204"',
+    title:"SHIRT STYLE!"
+},
+
+]
